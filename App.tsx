7e5e20ddc46665233049748fe120ce98d24fc219
Dhiraj Borse <borsedhiraj123@gmail.com>
2025-08-17 14:01:29 +0530

    extend web support

--- App.tsx
+++ App.tsx
@@ -7,27 +7,19 @@ import {NavigationContainer} from '@react-navigation/native';
 import {MainNavigator} from './src/navigation/MainNavigator';
 import Orientation from 'react-native-orientation-locker';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { StatusBar } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 import { COLORS } from './src/theme';
 
 
 
 const App = () => {
-  useEffect(() => {
-    Orientation.lockToPortrait(); 
-  }, []);
+  console.log("App rendered!");
   return (
-    <SafeAreaProvider>
-      <StatusBar backgroundColor={COLORS.bg1} barStyle="dark-content" />
-      <Provider store={store}>
-        <NavigationContainer>
-          <GestureHandlerRootView style={{ flex: 1 }}>
-          <MainNavigator />
-          </GestureHandlerRootView>
-        </NavigationContainer>
-      </Provider>
-    </SafeAreaProvider>
+    <View>
+      <Text style={{color: "black"}}>Hello Wefdfb!</Text>
+    </View>
   );
 };
+
 
 export default App;
